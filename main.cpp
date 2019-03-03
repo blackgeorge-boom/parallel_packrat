7682fbd4a710d39b4dbd7f4e715f7e0fcf5df0cc
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2019-03-03 22:04:14 +0200

    Added Non terminals and terminals

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ class Cell {
     int p;
 public:
     Cell() :r{Result::unknown}, p{-1} {};
-    ~Cell() {}
+    ~Cell() = default;
 
     Result res() const { return r; }
     void res(Result result) { r = result; }
@@ -32,13 +32,95 @@ std::ostream& operator<<(std::ostream& os, const Cell& c)
         default:
             os << "Something's wrong";
     }
-    os << "\"," << c.pos() << "} \n";
+    return os << "\"," << c.pos() << "} \n";
 }
+
+class NonTerminal {
+    std::string n;
+    int idx;
+public:
+    NonTerminal() :n{}, idx{-1} {};
+    ~NonTerminal() = default;
+    std::string name() const { return n; }
+    int index() const { return idx; }
+};
+
+std::ostream& operator<<(std::ostream& os, const NonTerminal& nt)
+{
+    return os << "{\"" << nt.name() << "\"," << nt.index() << "} \n";
+}
+
+class Terminal {
+    char c;
+public:
+    Terminal() :c{'0'} {};
+    ~Terminal() = default;
+    char name() const { return c; }
+};
+
+std::ostream& operator<<(std::ostream& os, const Terminal& t)
+{
+    return os << "{\'" << t.name()  << "\'} \n";
+}
+
+class Element {
+public:
+    virtual void accept(class Visitor& v) = 0;
+};
+
+class This: public Element {
+public:
+    void accept(Visitor& v) override;
+    std::string thiss() { return "This"; };
+};
+
+class That: public Element {
+public:
+    void accept(Visitor& v) override;
+    std::string that() { return "That"; };
+};
+
+class Visitor {
+public:
+    virtual void visit(This* e) = 0;
+    virtual void visit(That* e) = 0;
+};
+
+void This::accept(Visitor &v)
+{
+    v.visit(this);
+}
+
+void That::accept(Visitor &v)
+{
+    v.visit(this);
+}
+
+class UpVisitor: public Visitor {
+public:
+    void visit(This *e) override {
+        std::cout << "Do up on " << e->thiss() << std::endl;
+    }
+    void visit(That *e) override {
+        std::cout << "Do up on " << e->that() << std::endl;
+    }
+};
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     Cell c;
     std::cout << c;
+    NonTerminal nt;
+    std::cout << nt;
+    Terminal t;
+    std::cout << t;
+
+    Element* list[] = { new This(), new That() };
+
+    UpVisitor up;
+    list[0]->accept(up);
+    list[1]->accept(up);
+
     return 0;
 }
